dbe29d571248f37eff7cb79f1b14dc0e82898680
Dan-Laptop <danyeomans98@gmail.com>
2017-02-01 16:33:35 -0500

    Add sfml conversion header file (unimplemented)

---
+++ sfml_gmtk.h
@@ -0,0 +1,11 @@
+#ifndef _GMTK_SFML_GMTK_H_
+#define _GMTK_SFML_GMTK_H_
+
+#ifdef SFML_VERSION_MAJOR
+
+// implement sfml conversion cases here
+
+#endif//SFML_VERSION_MAJOR
+
+
+#endif//_GMTK_SFML_GMTK_H_
