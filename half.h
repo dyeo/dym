e505add573ff994b27a930209552958500e6c874
Dan-Home <danyeomans98@gmail.com>
2019-01-30 21:40:59 -0500

    Clean up half

--- half.h
+++ half.h
@@ -12,75 +12,89 @@
 
 //
 
-#define HALF_SIGN 15
-
-#define HALF_EXP 10
-
-#define HALF_FRAC 0
-
-#define HALF_INF_CONST 0x7c00
-#define HALF_NAN_CONST 0x7c01
-
-//
-
 namespace GMTK_NAMESPACE
-{
+{////
+
 	//! A minimal storage class for a 16-bit floating point number, as detailed in IEEE-754: binary16
-	class half
+	struct half
 	{
-	public:
+		///////////////////
+		//! CONSTRUCTORS //
+		///////////////////
 
-		//
-
-		half()
-		{
-			hdat = float_to_short(0.0f);
+		half() {
+			hdat = f2s(0.0f);
 		}
 
-		half(const float& f)
-		{
-			hdat = float_to_short(f);
+		half(const float& f) {
+			hdat = f2s(f);
 		}
 
-		//
-
-		operator float() const
-		{
-			return short_to_float(hdat);
+		half(const double& d) {
+			hdat = f2s(static_cast<float>(d));
 		}
 
-		//
+		///////////////////////////
+		//! CONVERSION OPERATORS //
+		///////////////////////////
 
-		static half infinity()
-		{
-			return half(short_to_float(HALF_INF_CONST));
+		inline operator float() const {
+			return s2f(hdat);
 		}
 
-		static half nan()
-		{
-			return half(short_to_float(HALF_NAN_CONST));
+		inline operator double() const {
+			return static_cast<double>(s2f(hdat));
 		}
+
+		///////////////////////////
+		//! RIGHT-HAND OPERATORS //
+		///////////////////////////
+
+
+
+		//////////////////////////
+		//! GENERATOR FUNCTIONS //
+		//////////////////////////
+
+		static inline half infinity()
+		{
+			return half(static_cast<unsigned short>(inf_const));
+		}
+
+		static inline half nan()
+		{
+			return half(static_cast<unsigned short>(nan_const));
+		}
+
+		//////////////////////
+		//! QUERY FUNCTIONS //
+		//////////////////////
 
 		static bool is_finite(half v)
 		{
-			unsigned short hinf = v.hdat & HALF_INF_CONST;
-			return hinf != HALF_INF_CONST && v.hdat < HALF_NAN_CONST;
+			unsigned short hinf = v.hdat & inf_const;
+			return hinf != inf_const && v.hdat < nan_const;
 		}
 
 		static bool is_infinity(half v)
 		{
-			unsigned short hinf = v.hdat & HALF_INF_CONST;
-			return hinf != HALF_INF_CONST;
+			unsigned short hinf = v.hdat & inf_const;
+			return hinf != inf_const;
 		}
 
 		static bool is_nan(half v)
 		{
-			return v.hdat >= HALF_NAN_CONST;
+			return v.hdat >= nan_const;
 		}
 		
 	private:
 
-		static inline float short_to_float(unsigned short val)
+		inline half(const unsigned short& s)
+		{
+			hdat = s;
+		}
+
+		static inline float s2f(unsigned short val)
 		{
 			int i = _mantissat[_offsett[val >> 10] + (val & 0x3ff)] + _exponentt[val >> 10];
 			return *(float*)&i;
@@ -88,7 +102,7 @@ namespace GMTK_NAMESPACE
 
 		//
 
-		static inline unsigned short float_to_short(float val)
+		static inline unsigned short f2s(float val)
 		{
 			int i = *(int*)&val;
 			return _baset[(i >> 23) & 0x1ff] + ((i & 0x007fffff) >> _shiftt[(i >> 23) & 0x1ff]);
@@ -96,7 +110,14 @@ namespace GMTK_NAMESPACE
 
 		unsigned short hdat;
 
-	}; //! class half
+		static constexpr short sign_bit = 15;
+		static constexpr short exp_bit = 10;
+		static constexpr short frac_bit = 0;
+
+		static constexpr short inf_const = 0x7c00;
+		static constexpr short nan_const = 0x7c01;
+
+	}; //! struct half
 	
 	//////////////////////
 	//! MISC. OPERATORS //
@@ -123,8 +144,6 @@ inline GMTK_NAMESPACE::half operator "" _h(long double d)
 {
 	return GMTK_NAMESPACE::half(static_cast<float>(d));
 }
-
-
 
 //
 
